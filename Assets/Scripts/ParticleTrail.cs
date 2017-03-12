3b2325c59753e3cc7c10afe34fa2091ed6c17999
Jan Kuczmowski <janqczmowski@gmail.com>
2017-03-12 23:44:09 +0100

    Added subtrails

--- Assets/Scripts/ParticleTrail.cs
+++ Assets/Scripts/ParticleTrail.cs
@@ -10,43 +10,56 @@ public class ParticleTrail : MonoBehaviour {
     private TrailRenderer[] trails;
     private ParticleSystem.Particle[] particles;
     private ParticleSystem.MainModule particleMain;
+    private Vector3 tPos;
+    private Vector3 pPos;
+    private TrailRenderer t;
+    private TrailRenderer tr;
+    private int maxParticlesCount;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         particleSystem = GetComponent<ParticleSystem>();
         particleMain = particleSystem.main;
-	}
+
+        if (particles == null || particles.Length < maxParticlesCount) particles = new ParticleSystem.Particle[10000];
+        if (trails == null || trails.Length < maxParticlesCount) trails = new TrailRenderer[10000];
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        int maxParticlesCount = particleMain.maxParticles;
-
-        if (particles == null || particles.Length < maxParticlesCount) particles = new ParticleSystem.Particle[maxParticlesCount];
-        if (trails == null || trails.Length < maxParticlesCount) trails = new TrailRenderer[maxParticlesCount];
-
+        maxParticlesCount = particleMain.maxParticles;
         particleSystem.GetParticles(particles);
 
         for (int i = 0; i < particles.Length; i++)
         {
             if (trails.Length - 1 < i || trails[i] == null)
             {
-                TrailRenderer t = Instantiate(trail);
+                t = Instantiate(trail);
                 trails[i] = t;
             }
 
-            Vector3 tPos = trails[i].transform.position;
-            Vector3 pPos = particles[i].position;
+            tPos = trails[i].transform.position;
+            pPos = particles[i].position;
             if (Vector3.Distance(tPos, pPos) > 1)
             {
-                TrailRenderer tr = trails[i];             
+                tr = trails[i];             
                 trails[i] = null;
-                Destroy(tr);
+                //Destroy(tr);
             }else
             {
                 trails[i].transform.position = particles[i].position;
             }
         }
+    }
+
+    private void OnDisable()
+    {
+        //for(int i = 0;i< trails.Length; i++)
+        //{
+        //    Destroy(trails[i].gameObject);
+        //}
+        //trails = null;
     }
 }
